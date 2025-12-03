bbea6340d21c1cd1d730984c5ccf1bb76cb50049
Jerson-Cortes <jersoncortes.work@proton.me>
2025-12-02 23:43:39 -0600

    Update footer links to social media and website metadata.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,46 +48,46 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/jerson-cortes-palomino/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/linkedin.svg"
             alt="File icon"
             width={16}
             height={16}
           />
-          Learn
+          LinkedIn
         </a>
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="mailto:jersoncortes.work@protonmail.com?subject=Feedback%20From%20GitHub%20Pages&body=Hello,"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/email.svg"
             alt="Window icon"
             width={16}
             height={16}
           />
-          Examples
+          Email
         </a>
         <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://github.com/Jerson-Cortes"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="/github.svg"
             alt="Globe icon"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          GitHub
         </a>
       </footer>
     </div>
