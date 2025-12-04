8c18fdfb73ebe18c36a87ba25e3026fa358d1c18
Jerson-Cortes <jersoncortes.work@proton.me>
2025-12-03 23:03:26 -0600

    Add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local'
+
+const switzer = localFont({
+  src: '../../public/fonts/switzer/Switzer-Regular.woff2',
+  variable: "--font-geist-sans",
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${switzer.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
