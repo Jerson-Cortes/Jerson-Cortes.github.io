ef706dc6cd1635d9d03ff88c5531fe2885b74168
Jerson-Cortes <jersoncortes.work@proton.me>
2025-12-15 12:51:27 -0600

    Fix footer not having background color.

--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex fixed inset-x-0 bottom-0 p-5">
+    <footer className="flex fixed inset-x-0 bottom-0 p-5 bg-white">
       <p className="text-lg bg-(--red)">MEXICO</p>
       <div className="ml-auto space-x-3 text-lg">
         <a href="https://github.com/Jerson-Cortes" target="_blank" rel="noopener noreferrer">GitHub</a>
