38756e9b36517958a1fc05f882b86fbb3edf954b
Jerson-Cortes <jersoncortes.work@proton.me>
2025-12-15 00:39:07 -0600

    Add supported japanese font. Add footer links. Fixed sizing issue in japanese and software text.

--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,20 @@ export default function Home() {
         <div className="flex flex-col">
           <h1 className="text-(--red) text-7xl order-0">JERSON</h1>
           <h1 className="text-(--red) text-7xl order-2 absolute top-25 right-4">CORTES</h1>
-          <h3 className="order-1 writing-vertical-lr py-4">努力は裏切らない</h3>
-          <h3 className="order-3 absolute top-41 right-15">SOFTWARE DEVELOPER</h3>
-          <div className="flex flex-col order-4 py-32 right-5 ml-auto space-y-8">
+          <h3 className="text-4xl order-1 writing-vertical-lr py-4 px-4 japanese">努力は裏切らない</h3>
+          <h3 className="text-lg order-3 absolute top-41 right-11">SOFTWARE DEVELOPER</h3>
+          <div className="flex flex-col order-4 py-12 right-5 ml-auto space-y-8">
             <h2 className="text-4xl ml-auto">[ABOUT ME]</h2>
             <h2 className="text-4xl ml-auto">[SKILLS]</h2>
             <h2 className="text-4xl ml-auto">[CONTACT]</h2>
           </div>
         </div>
-        <footer className="flex justify-between">
-          <div className="">
-            <p>MEXICO</p>
-          </div>
-          <div className="">
-            <a href="">GitHub</a>
-            <a href="">LinkedIn</a>
-            <a href="">Email</a>
+        <footer className="flex fixed inset-x-0 bottom-0 p-5">
+            <p className="text-lg bg-(--red)">MEXICO</p>
+          <div className="ml-auto space-x-3 text-lg">
+            <a href="https://github.com/Jerson-Cortes" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://www.linkedin.com/in/jerson-cortes-palomino" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="mailto:JersonCortes.work@proton.me?subject=Feedback%20From%20Github&body=Hello," target="_blank" rel="noopener noreferrer">Email</a>
           </div>
         </footer>
       </main>
