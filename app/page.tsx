0273024c6251fbf37254c58c68f10695ceed5f84
Jerson-Cortes <jersoncortes.work@proton.me>
2025-12-15 16:44:11 -0600

    Add about-me section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./sections/hero"
 import Footer from "./sections/footer"
+import AboutMe from "./sections/about_me"
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center dark:bg-white">
-      <main className="flex min-h-screen w-full flex-col justify-between p-4 dark:bg-white sm:items-start">
+      <main className="flex min-h-screen w-full flex-col justify-between p-5 dark:bg-white sm:items-start">
         <Hero/>
+        <AboutMe/>
         <Footer/>
       </main>
     </div>
