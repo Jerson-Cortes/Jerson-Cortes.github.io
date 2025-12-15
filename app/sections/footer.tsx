7d415e269bc9243eb1c284db3829d8e778637b5c
Jerson-Cortes <jersoncortes.work@proton.me>
2025-12-15 17:28:33 -0600

    Fix font overwriting <a>.Refactor hero and footer <a>'s

--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex fixed inset-x-0 bottom-0 p-5 bg-white">
       <p className="text-lg bg-(--red) font-bold text-white">MEXICO</p>
-      <div className="ml-auto space-x-3 text-lg">
+      <div className="ml-auto space-x-3 text-lg font-bold">
         <a href="https://github.com/Jerson-Cortes" target="_blank" rel="noopener noreferrer">GitHub</a>
         <a href="https://www.linkedin.com/in/jerson-cortes-palomino" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         <a href="mailto:JersonCortes.work@proton.me?subject=Feedback%20From%20Github&body=Hello," target="_blank" rel="noopener noreferrer">Email</a>
