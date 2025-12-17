52d8bd9d41bae685161afc2a62240980a9252f61
Jerson-Cortes <jersoncortes.work@proton.me>
2025-12-17 17:27:16 -0600

    Add sections tag to each one.

--- app/sections/about_me.tsx
+++ app/sections/about_me.tsx
@@ -1,27 +1,29 @@
 export default function AboutMe() {
   return (
-    <div className="flex flex-col pt-10 relative" id="AboutMe">
-      <h2 className="text-(--red) text-4xl">[ABOUT ME]</h2>
-      <p className="indent-8 py-8">"Creating tools that deliver rich, user‑friendly experiences while safeguarding privacy."</p>
+    <section id="AboutMe">
+      <div className="flex flex-col pt-10 relative" id="AboutMe">
+        <h2 className="text-(--red) text-4xl">[ABOUT ME]</h2>
+        <p className="indent-8 py-8">"Creating tools that deliver rich, user‑friendly experiences while safeguarding privacy."</p>
 
-      <div className="pr-18">
-        <div className="flex items-center">
-          <p className="font-bold text-lg pr-4">WHAT I DO</p>
-          <hr className="flex-1" />
+        <div className="pr-18">
+          <div className="flex items-center">
+            <p className="font-bold text-lg pr-4">WHAT I DO</p>
+            <hr className="flex-1" />
+          </div>
+          <p className="indent-8 pb-6">Develop beautiful, clean applications with advanced functionality for power users while remaining intuitive for non-technical users to master over time.</p>
+          <div className="flex items-center">
+            <p className="font-bold text-lg pr-4">I AM</p>
+            <hr className="flex-1" />
+          </div>
+          <p className="indent-8 pb-6">A perfectionist who pays attention to every detail be it functional or design wise. I advocate for privacy concious applications and contribute to open-source projects.</p>
+          <div className="flex items-center">
+            <p className="font-bold text-lg pr-4">I LIKE</p>
+            <hr className="flex-1" />
+          </div>
+          <p className="indent-8 pb-6">To learn languages, at the moment I'm focusing on eastern ones. Discover about the cosmos. Read about anything and everything. Sport bikes.</p>
         </div>
-        <p className="indent-8 pb-6">Develop beautiful, clean applications with advanced functionality for power users while remaining intuitive for non-technical users to master over time.</p>
-        <div className="flex items-center">
-          <p className="font-bold text-lg pr-4">I AM</p>
-          <hr className="flex-1" />
-        </div>
-        <p className="indent-8 pb-6">A perfectionist who pays attention to every detail be it functional or design wise. I advocate for privacy concious applications and contribute to open-source projects.</p>
-        <div className="flex items-center">
-          <p className="font-bold text-lg pr-4">I LIKE</p>
-          <hr className="flex-1" />
-        </div>
-        <p className="indent-8 pb-6">To learn languages, at the moment I'm focusing on eastern ones. Discover about the cosmos. Read about anything and everything. Sport bikes.</p>
+        <p className="absolute right-1 top-50 text-4xl japanese writing-vertical-lr">継続は力なり</p>
       </div>
-      <p className="absolute right-1 top-50 text-4xl japanese writing-vertical-lr">継続は力なり</p>
-    </div>
+    </section>
   )
 }
--- app/sections/contact.tsx
+++ app/sections/contact.tsx
@@ -1,53 +1,55 @@
 export default function Contact() {
   return (
-    <div className="flex flex-col pt-10 pb-15 relative " id="AboutMe">
-      <h2 className="text-(--red) text-4xl pb-6">[CONTACT]</h2>
-      <p>I’m always open to opportunities, be it freelance, contract, or cool projects so reach out!</p>
-      <form className="flex flex-col pt-6 pb-16">
-        <div className="flex justify-between gap-2">
+    <section id="Contact">
+      <div className="flex flex-col pt-10 pb-15 relative " id="AboutMe">
+        <h2 className="text-(--red) text-4xl pb-6">[CONTACT]</h2>
+        <p>I’m always open to opportunities, be it freelance, contract, or cool projects so reach out!</p>
+        <form className="flex flex-col pt-6 pb-16">
+          <div className="flex justify-between gap-2">
+            <div className="flex flex-col mb-4">
+              <label className="font-bold">Name</label>
+              <input
+                id="name"
+                type="text"
+                required
+                className=" w-full border border-black focus:outline-none"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="font-bold">Email</label>
+              <input
+                id="name"
+                type="text"
+                required
+                className="w-full border border-black focus:outline-none"
+              />
+            </div>
+          </div>
           <div className="flex flex-col mb-4">
-            <label className="font-bold">Name</label>
+            <label className="font-bold">Subject</label>
             <input
               id="name"
               type="text"
               required
-              className=" w-full border border-black focus:outline-none"
+              className="border border-black focus:outline-none"
             />
           </div>
-          <div className="flex flex-col">
-            <label className="font-bold">Email</label>
-            <input
-              id="name"
-              type="text"
+          <div className="flex flex-col mb-4">
+            <label className="font-bold">Message</label>
+            <textarea
+              id="details"
               required
-              className="w-full border border-black focus:outline-none"
-            />
+              className="h-32 border border-black focus:outline-none resize-none"
+            >
+            </textarea>
           </div>
-        </div>
-        <div className="flex flex-col mb-4">
-          <label className="font-bold">Subject</label>
           <input
-            id="name"
-            type="text"
-            required
-            className="border border-black focus:outline-none"
+            type="submit"
+            className={`bg-(--red) amulya-bolditalic text-white text-lg ml-auto my-4 p-0.5`}
+            value="SEND MESSAGE"
           />
-        </div>
-        <div className="flex flex-col mb-4">
-          <label className="font-bold">Message</label>
-          <textarea 
-            id="details" 
-            required 
-            className="h-32 border border-black focus:outline-none resize-none"
-          >
-          </textarea>
-        </div>
-        <input 
-          type="submit" 
-          className={`bg-(--red) amulya-bolditalic text-white text-lg ml-auto my-4 p-0.5`} 
-          value="SEND MESSAGE"
-        />
-      </form>
-    </div>
+        </form>
+      </div>
+    </section>
   )
 }
