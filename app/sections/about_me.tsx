dc24a862f45ff54f58f7378ca2ac4a5b671bdffe
Jerson-Cortes <jersoncortes.work@proton.me>
2025-12-18 17:31:36 -0600

    Add table responsiveness to about-me section.

--- app/sections/about_me.tsx
+++ app/sections/about_me.tsx
@@ -1,28 +1,33 @@
 export default function AboutMe() {
   return (
-    <section id="AboutMe">
-      <div className="flex flex-col mt-18 relative" id="AboutMe">
-        <h2 className="text-(--red) text-4xl">[ABOUT ME]</h2>
-        <p className="indent-8 py-8">"Creating tools that deliver rich, user‑friendly experiences while safeguarding privacy."</p>
-
-        <div className="pr-18">
-          <div className="flex items-center">
-            <p className="font-bold text-lg pr-4">WHAT I DO</p>
-            <hr className="flex-1" />
+    <section id="AboutMe" className="w-full md:h-[calc(100vh-6rem)]">
+      <div className="flex flex-col mt-22 relative" id="AboutMe">
+        <h2 className="text-(--red) text-4xl md:ml-20">[ABOUT ME]</h2>
+        <p className="indent-8 py-8 md:w-100 md:mt-5 md:ml-20">"Creating tools that deliver rich, user‑friendly experiences while safeguarding privacy."</p>
+        <div className="flex flex-col md:flex-row md:flex-wrap pr-18 md:px-45 md:justify-center md:mt-4">
+          <div className="md:w-1/2 md:px-2">
+            <div className="flex items-center">
+              <p className="font-bold text-lg pr-4">WHAT I DO</p>
+              <hr className="flex-1" />
+            </div>
+            <p className="indent-8 pb-6">Develop beautiful, clean applications with advanced functionality for power users while remaining intuitive for non-technical users to master over time.</p>
           </div>
-          <p className="indent-8 pb-6">Develop beautiful, clean applications with advanced functionality for power users while remaining intuitive for non-technical users to master over time.</p>
-          <div className="flex items-center">
-            <p className="font-bold text-lg pr-4">I AM</p>
-            <hr className="flex-1" />
+          <div className="md:w-1/2 md:px-2">
+            <div className="flex items-center">
+              <p className="font-bold text-lg pr-4">I AM</p>
+              <hr className="flex-1" />
+            </div>
+            <p className="indent-8 pb-6">A perfectionist who pays attention to every detail be it functional or design wise. I advocate for privacy concious applications and contribute to open-source projects.</p>
           </div>
-          <p className="indent-8 pb-6">A perfectionist who pays attention to every detail be it functional or design wise. I advocate for privacy concious applications and contribute to open-source projects.</p>
-          <div className="flex items-center">
-            <p className="font-bold text-lg pr-4">I LIKE</p>
-            <hr className="flex-1" />
+          <div className="md:w-1/2">
+            <div className="flex items-center">
+              <p className="font-bold text-lg pr-4">I LIKE</p>
+              <hr className="flex-1" />
+            </div>
+            <p className="indent-8 pb-6">To learn languages, at the moment I'm focusing on eastern ones. Discover about the cosmos. Read about anything and everything. Sport bikes.</p>
           </div>
-          <p className="indent-8 pb-6">To learn languages, at the moment I'm focusing on eastern ones. Discover about the cosmos. Read about anything and everything. Sport bikes.</p>
         </div>
-        <p className="absolute right-1 top-50 text-4xl japanese writing-vertical-lr">継続は力なり</p>
+        <p className="absolute right-1 top-50 text-4xl japanese writing-vertical-lr md:right-20 md:top-40">継続は力なり</p>
       </div>
     </section>
   )
