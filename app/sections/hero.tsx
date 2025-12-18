646cbc7359bbffd0eba9b5d5e85f8066ac3c1005
Jerson-Cortes <jersoncortes.work@proton.me>
2025-12-17 21:22:55 -0600

    Fix Hero links to sections.

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -7,9 +7,9 @@ export default function Hero() {
         <h3 className="text-4xl order-1 writing-vertical-lr py-4 px-4 japanese">努力は裏切らない</h3>
         <h3 className="text-lg order-3 absolute top-41 right-11">SOFTWARE DEVELOPER</h3>
         <div className="flex flex-col order-4 py-12 right-5 ml-auto space-y-8 font-bold">
-          <a className="text-4xl ml-auto hover:text-(--red)" href="/">[ABOUT ME]</a>
-          <a className="text-4xl ml-auto hover:text-(--red)" href="/">[SKILLS]</a>
-          <a className="text-4xl ml-auto hover:text-(--red)" href="/">[CONTACT]</a>
+          <a className="text-4xl ml-auto hover:text-(--red)" href="#AboutMe">[ABOUT ME]</a>
+          <a className="text-4xl ml-auto hover:text-(--red)" href="#Skills">[SKILLS]</a>
+          <a className="text-4xl ml-auto hover:text-(--red)" href="#Contact">[CONTACT]</a>
         </div>
       </div>
     </section>
